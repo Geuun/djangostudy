b2e13d67a7c97b8112398fa353100cb6a493590e
KimJaeGeun <jgk9612@gmail.com>
2022-06-21 16:06:32 +0900

    feat: Start shortener app
    Add base shortener app files

---
+++ shortener/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+
+class PayPlan(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    updated_at = models.DateTimeField(auto_now=True)
+    create_at = models.DateTimeField(auto_now_add=True)
