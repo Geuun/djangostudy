dbe269a9010f360f2bfbb28d974c1f032ff3d6e7
KimJaeGeun <jgk9612@gmail.com>
2022-06-21 21:09:12 +0900

    feat: Add AbstractUser
    220621

--- shortener/models.py
+++ shortener/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User as U
+from django.contrib.auth.models import AbstractUser
 
 
 
@@ -7,3 +9,10 @@ class PayPlan(models.Model):
     price = models.IntegerField()
     updated_at = models.DateTimeField(auto_now=True)
     create_at = models.DateTimeField(auto_now_add=True)
+    
+# class Users(AbstractUser):
+#     pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING)
+    
+class UserDetail(models.Model):
+    user = models.OneToOneField(U, on_delete=models.CASCADE)
+    pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING)
