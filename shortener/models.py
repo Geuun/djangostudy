d17a76e1dae02688884aadb8938db08f5cec9fdc
KimJaeGeun <jgk9612@gmail.com>
2022-06-21 21:25:09 +0900

    fix: change userdetail to abstractuser
    db 단순화를 위해 abstractuser 반영

--- shortener/models.py
+++ shortener/models.py
@@ -10,9 +10,9 @@ class PayPlan(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     create_at = models.DateTimeField(auto_now_add=True)
     
-# class Users(AbstractUser):
-#     pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING)
-    
-class UserDetail(models.Model):
-    user = models.OneToOneField(U, on_delete=models.CASCADE)
+class Users(AbstractUser):
     pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING)
+    
+# class UserDetail(models.Model):
+#     user = models.OneToOneField(U, on_delete=models.CASCADE)
+#     pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING)
