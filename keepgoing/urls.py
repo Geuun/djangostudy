b08ed36cbd6f2e9c7a8c29c16073831cc39e4c29
KimJaeGeun <jgk9612@gmail.com>
2022-06-23 17:49:29 +0900

    feat: Add Django URLs and Views and Models ver2
    URLs, Views, Models에 대한 코드를 추가하였습니다.

--- keepgoing/urls.py
+++ keepgoing/urls.py
@@ -1,4 +1,4 @@
-from shortener.views import index, redirect_test, get_user
+from shortener.views import index, get_user#, redirect_test
 from django.contrib import admin
 from django.urls import path
 
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='homes' ),
     #path('redirect', redirect_test),
-    pathr('get_user/<int:user_id>', get_user)
+    path('get_user/<int:user_id>', get_user)
 ]
--- shortener/views.py
+++ shortener/views.py
@@ -1,5 +1,6 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
-
 from shortener.models import Users
 
 # Create your views here.
@@ -9,6 +10,10 @@ def index(request):
     email = user.email if user else 'Anomymous User!'
     return render(request, 'base.html', {'welcome_msg': f'Hello {email}'})
 
+# def redirect_test(request):
+#     print('Go Redirect')
+#     return redirect('home')
+
 @csrf_exempt
 def get_user(request, user_id):
     print(user_id)
@@ -17,7 +22,9 @@ def get_user(request, user_id):
         xyz = request.GET.get('xyz')
         user = Users.objects.filter(pk=user_id).first()
         return render(request, 'base.html', {'user': user, 'params': [abc, xyz]})
+    elif request.method == "POST":
+        username = request.GET.get('username')
+        if username:
+            user = Users.objects.filter(pk=user_id).update(username=username)
+        return JsonResponse(status=201, data=dict(msg='You just reached with Post Method!'), safe=False)
 
-# def redirect_test(request):
-#     print('Go Redirect')
-#     return redirect('home')
