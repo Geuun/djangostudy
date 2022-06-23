88e90cd7151d4b434db909458db0edb5c90bf32a
KimJaeGeun <jgk9612@gmail.com>
2022-06-23 17:12:00 +0900

    feat: Add Django URLs and Views and Models base
    URLs, Views, Models 의 base code를 추가하였습니다.

--- keepgoing/urls.py
+++ keepgoing/urls.py
@@ -1,9 +1,10 @@
-from shortener.views import index, redirect_test
+from shortener.views import index, redirect_test, get_user
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='home' ),
-    path('redirect', redirect_test),
+    path('', index, name='homes' ),
+    #path('redirect', redirect_test),
+    pathr('get_user/<int:user_id>', get_user)
 ]
--- shortener/views.py
+++ shortener/views.py
@@ -4,16 +4,20 @@ from shortener.models import Users
 
 # Create your views here.
 def index(request):
-    # user = Users.objects.filter(username='admin').first()
-    user = User.object.get(username='admin')
+    user = Users.objects.filter(username='admin').first()
+    # user = User.object.get(username='admin')
     email = user.email if user else 'Anomymous User!'
-    print(email)
-    print(request.user.is_authenticated)
-    if request.user.is_authenticated is False:
-        email = 'Anomymous User!'
-    print(email)
     return render(request, 'base.html', {'welcome_msg': f'Hello {email}'})
 
-def redirect_test(request):
-    print('Go Redirect')
-    return redirect('home')
+@csrf_exempt
+def get_user(request, user_id):
+    print(user_id)
+    if request.method == 'GET':
+        abc = request.GET.get('abc')
+        xyz = request.GET.get('xyz')
+        user = Users.objects.filter(pk=user_id).first()
+        return render(request, 'base.html', {'user': user, 'params': [abc, xyz]})
+
+# def redirect_test(request):
+#     print('Go Redirect')
+#     return redirect('home')
