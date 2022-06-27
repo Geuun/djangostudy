ef119b54711adf5d777ae451f88bc0705c520af4
KimJaeGeun <jgk9612@gmail.com>
2022-06-27 20:20:49 +0900

    feat: Add register, login, logout, forms
    회원가입, 로그인, 로그아웃 forms를 추가하였습니다.

--- keepgoing/urls.py
+++ keepgoing/urls.py
@@ -1,15 +1,16 @@
 import debug_toolbar
 from django.conf.urls import include
-from shortener.views import index, get_user#, redirect_test
+from shortener.views import index, get_user, list_view, register, login_view, logout_view
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('__debug__/', include(debug_toolbar.urls)),
-    path('', index, name='homes' ),
-    #path('redirect', redirect_test),
-    path('get_user/<int:user_id>', get_user)
+    path("admin/", admin.site.urls),
+    path("__debug__/", include(debug_toolbar.urls)),  # Django Debug Tool
+    path("", index, name="index"),
+    path("register", register, name="register"),
+    path("login", login_view, name="login"),
+    path("logout", logout_view, name="logout"),
+    path("list", list_view, name="list_view"),
+    path("get_user/<int:user_id>", get_user),
 ]
